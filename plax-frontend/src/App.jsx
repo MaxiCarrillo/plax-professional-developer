db7911ba21a657005253ee72dfbbf0caab362651
MaxiCarrillo <maxi11carrill@gmail.com>
2024-09-08 12:21:20 -0300

    Add TopMenu

---
+++ plax-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { TopMenu } from "./components/TopMenu"
+
+function App() {
+  return (
+    <>
+      <TopMenu />
+      <div style={{ height: "200vh" }}>
+        dsadsaddddddddd
+      </div>
+    </>
+  )
+}
+
+export default App
